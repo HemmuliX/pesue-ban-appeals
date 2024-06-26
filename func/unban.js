b0aa79f2ab7bdabfe5aa4b4bb7d2d3311bb11156
HemmuliX <54975450+HemmuliX@users.noreply.github.com>
2024-06-26 07:51:09 +0300

    Update unban.js

--- func/unban.js
+++ func/unban.js
@@ -17,14 +17,14 @@ export async function handler(event, context) {
                 return {
                     statusCode: 303,
                     headers: {
-                        "Location": `/success?msg=${encodeURIComponent("User has been unbanned\nPlease contact them and let them know")}`
+                        "Location": `/success?msg=${encodeURIComponent("Käyttäjän porttikielto on nyt poistettu\nOta käyttäjään yhteyttä ja kerro asiasta.")}`
                     }
                 };
             } catch (e) {
                 return {
                     statusCode: 303,
                     headers: {
-                        "Location": `/error?msg=${encodeURIComponent("Failed to unban user\nPlease manually unban")}`
+                        "Location": `/error?msg=${encodeURIComponent("Porttikiellon poisto epäonnistui\nVoisitko tehdä sen manuaalisesti")}`
                     }
                 };
             }
